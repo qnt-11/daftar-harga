dd3781db9d2a07b41197786f9bf58163b088e4ff
Barla Fam <nd4zmoe@gmail.com>
2026-05-13 23:23:18 +0700

    Update app version from 6.1 to 7.0

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * SERVICE WORKER DAFTAR HARGA - BULLETPROOF ENTERPRISE GRADE
  */
 
-const APP_VERSION = '6.1'; 
+const APP_VERSION = '7.0'; 
 const CACHE_CORE = 'core-v' + APP_VERSION; 
 const CACHE_DYNAMIC = 'dyn-v' + APP_VERSION;
 const CACHE_CDN = 'cdn-v1'; 
