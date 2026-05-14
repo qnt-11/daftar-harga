3c79dac3ab1b6aeabc6514ab50a886b8098ce253
Barla Fam <nd4zmoe@gmail.com>
2026-05-15 00:37:11 +0700

    Update app version to 9.0 in service worker

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * SERVICE WORKER DAFTAR HARGA - BULLETPROOF ENTERPRISE GRADE
  */
 
-const APP_VERSION = '8.9'; 
+const APP_VERSION = '9.0'; 
 const CACHE_CORE = 'core-v' + APP_VERSION; 
 const CACHE_DYNAMIC = 'dyn-v' + APP_VERSION;
 const CACHE_CDN = 'cdn-v1'; 
