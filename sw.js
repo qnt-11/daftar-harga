1609fde4d8fa59e5db13954f0784acd86d4a1a31
Barla Fam <nd4zmoe@gmail.com>
2026-05-14 19:11:52 +0700

    Update app version from 8.6 to 8.7

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * SERVICE WORKER DAFTAR HARGA - BULLETPROOF ENTERPRISE GRADE
  */
 
-const APP_VERSION = '8.6'; 
+const APP_VERSION = '8.7'; 
 const CACHE_CORE = 'core-v' + APP_VERSION; 
 const CACHE_DYNAMIC = 'dyn-v' + APP_VERSION;
 const CACHE_CDN = 'cdn-v1'; 
